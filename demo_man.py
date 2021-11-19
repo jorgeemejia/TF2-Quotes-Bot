e18a4d5a19c63494cc63198782ee29e3f24572c6
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2021-11-18 23:55:57 -0800

    Added 9 demo_man lines

--- demo_man.py
+++ demo_man.py
@@ -15,7 +15,16 @@ demo_man_dict = {"Yer a back-pokin' snake, and by God you'll die like one!" : "h
         "Little too much caber-tossin' pie down yer own throat, eh, chubby?" : "https://www.youtube.com/watch?v=THIDhZDbby0",
         "Wizard! Show yerself!" : "https://www.youtube.com/watch?v=02-HU11qnys",
         "He's got that book that stole me eye!" : "https://www.youtube.com/watch?v=f_N7vIl-EGo",
-        "Not one of ya's going to survive this!" : "https://www.youtube.com/watch?v=E35gJNViNIY"
+        "Not one of ya's going to survive this!" : "https://www.youtube.com/watch?v=E35gJNViNIY",
+        "We're a sorry buncha' losers!" : "https://www.youtube.com/watch?v=dWHz3I1WcnI",
+        "In your language; 'Eat lead, laddies!" : "https://www.youtube.com/watch?v=fGgnUGNYPmg",
+        "You did good!" : "https://www.youtube.com/watch?v=JGluunMO-bA",
+        "Hah! Barely broke a sweat!" : "https://www.youtube.com/watch?v=O9YLwyck19w",
+        "FREEDOM!" : "https://www.youtube.com/watch?v=D2Ez1tAzSIw",
+        "That wasn't supposed ta' happen!" : "https://www.youtube.com/watch?v=ixBfsjUNXtw",
+        "Whoop de doo!" : "https://www.youtube.com/watch?v=vS2yNN1Lc_Y",
+        "Aye, me bottle o' scrumpy!" : "https://www.youtube.com/watch?v=cPXQDR6o3Kg",
+        "Yer a devil! A devil!" : "https://www.youtube.com/watch?v=REtGUu0YOiQ"
         }
 
 
